2d9decf994112143f94347ef5cc691112bddbf41
Michael Shirinyan <mshirinyan@outlook.com>
2021-03-21 19:33:27 +0400

    Add project files.

---
+++ NORTHWND.Core/Entities/GetRandView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NORTHWND.Core.Entities
+{
+    public partial class GetRandView
+    {
+        public double? Value { get; set; }
+    }
+}
---
+++ NORTHWND.DAL/Configurations/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NORTHWND.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NORTHWND.DAL.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Email).HasColumnType("VARCHAR(100)");
+            builder.Property(x => x.Password).HasColumnType("VARCHAR(100)");
+        }
+
+    }
+}
