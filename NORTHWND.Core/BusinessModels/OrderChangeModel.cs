e4ed1f41493c18a16080e4b974b2774386316d63
Michael Shirinyan <mshirinyan@outlook.com>
2021-04-01 17:15:57 +0400

    Fixed OrderChangeModel

--- NORTHWND.Core/BusinessModels/OrderChangeModel.cs
+++ NORTHWND.Core/BusinessModels/OrderChangeModel.cs
@@ -14,9 +14,9 @@ namespace NORTHWND.Core.BusinessModels
 ErrorMessage = "Characters are not allowed")]
         public string CustomerId { get; set; }
         [Range(1, 1000)]
-        public int EmployeeId { get; set; }
+        public int? EmployeeId { get; set; }
         [Range(1, 10000)]
-        public decimal Freight { get; set; }
+        public decimal? Freight { get; set; }
         [RegularExpression(@"^[a-zA-Z''-'\s]{2,15}$",
             ErrorMessage = "Characters are not allowed")]
         public string ShipName { get; set; }
